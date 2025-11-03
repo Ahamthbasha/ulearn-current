9441f65e2b5f8832e83d046625d55487ef8483c4
ahamathbasha <bashachristopherallah786@gmail.com>
2025-11-03 19:28:32 +0530

    MADE the changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: 'public',
   server: {
     allowedHosts: [
       '.ngrok-free.app', 
