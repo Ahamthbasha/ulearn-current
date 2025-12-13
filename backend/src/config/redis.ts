ea143a3a3d473a4dc27b492c6b73a54a17eee478
Ahamathbasha <bashachristopherallah786@gmail.com>
2025-12-13 20:34:22 +0530

    redis issue try to resolve

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,12 +1,30 @@
+// import Redis from "ioredis";
+// import { appLogger } from "../utils/logger";
+
+// const redisClient = process.env.REDIS_URL
+//   ? new Redis(process.env.REDIS_URL)
+//   : new Redis({
+//       host: process.env.REDIS_HOST || "127.0.0.1",
+//       port: Number(process.env.REDIS_PORT) || 6379,
+//     });
+
+// redisClient.on("connect", () => {
+//   appLogger.info("Connected to Redis");
+// });
+
+// redisClient.on("error", (err) => {
+//   appLogger.error("Redis Client Error:", err);
+// });
+
+// export default redisClient;
+
 import Redis from "ioredis";
 import { appLogger } from "../utils/logger";
 
-const redisClient = process.env.REDIS_URL
-  ? new Redis(process.env.REDIS_URL)
-  : new Redis({
-      host: process.env.REDIS_HOST || "127.0.0.1",
-      port: Number(process.env.REDIS_PORT) || 6379,
-    });
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: Number(process.env.REDIS_PORT) || 6379,
+});
 
 redisClient.on("connect", () => {
   appLogger.info("Connected to Redis");
